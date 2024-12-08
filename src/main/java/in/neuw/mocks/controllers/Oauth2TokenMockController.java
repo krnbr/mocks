9b6c043270f08a6015c5fef77ab2117833e65578
Karanbir Singh <krnbr@live.in>
2024-12-08 21:39:48 +0530

    minor tweaks

--- src/main/java/in/neuw/mocks/controllers/Oauth2TokenMockController.java
+++ src/main/java/in/neuw/mocks/controllers/Oauth2TokenMockController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
--- src/main/java/in/neuw/mocks/controllers/ResourceController.java
+++ src/main/java/in/neuw/mocks/controllers/ResourceController.java
@@ -1,14 +1,20 @@
 package in.neuw.mocks.controllers;
 
 import in.neuw.mocks.models.HealthStatus;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ResourceController {
 
     @GetMapping("/ping")
-    public HealthStatus ping() {
+    public HealthStatus ping(@RequestHeader("Authorization") String token,
+                             HttpServletRequest request) {
+        log.info("input Authorization - {} on port - {}", token, request.getServerPort());
         return new HealthStatus().setStatus("pong");
     }
 
